1582518545b718710037348538601a2004d19dc3
mete kabakci <kabakcimete@gmail.com>
2019-08-29 18:06:28 +0300

    61.video 13:00

---
+++ MyShop2/MyShop2.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop2.core.Models;
+
+namespace MyShop2.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        private ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products==null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if (productToUpdate == null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+
+        }
+    }
+}
