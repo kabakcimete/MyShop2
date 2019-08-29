f7f54690df46c582a2bd85633bcc497c9368656b
mete kabakci <kabakcimete@gmail.com>
2019-08-29 16:04:39 +0300

    initial solution setıp

---
+++ MyShop2/MyShop2.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop2.WebUI.Startup))]
+namespace MyShop2.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
